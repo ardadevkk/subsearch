64cc91c4b52ab40823179cb794920138d0170367
Arda <124434945+knqsec@users.noreply.github.com>
2023-05-07 02:30:09 +0300

    Update subsearch.py
    Added Output Saving

--- subsearch.py
+++ subsearch.py
@@ -1,11 +1,14 @@
 import requests
 import sys
+import os
 
 if len(sys.argv) != 2:
-    print(f"Kullanım: python {sys.argv[0]} <domain>")
+    print(f"Usage: python {sys.argv[0]} <domain>")
     sys.exit(1)
 
 domain = sys.argv[1]
+output_dir = "output"
+output_file = os.path.join(output_dir, f"{domain}.txt")
 
 try:
     print('Scanned Al Subdomains Wait please...')
@@ -15,6 +18,9 @@ try:
         subdomains.add(certificate["name_value"])
     for subdomain in sorted(subdomains):
         print(subdomain)
+    with open(output_file, "w") as f:
+        for subdomain in sorted(subdomains):
+            f.write(subdomain + "\n")
 except requests.exceptions.RequestException as e:
-    print(f"Error: {e}")
+    print(f"Hata: {e}")
     sys.exit(1)
